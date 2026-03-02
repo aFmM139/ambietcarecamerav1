fcbcb306de972928cebf452191d35fd9148ddb89
andyfajardo1309 <andyfajardo1309@gmail.com>
2026-03-02 17:25:00 +0000

    ✨ feat(_layout):Se deshabilitan gestos -Se deshabilitan los gestos del celular

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="CameraScreen" options={{ headerShown: false }} />
-      <Stack.Screen name="ServerScreen" options={{ headerShown: false }} />
-      <Stack.Screen name="TableScreen" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false, gestureEnabled: false }} />
+      <Stack.Screen name="CameraScreen" options={{ headerShown: false, gestureEnabled: false }} />
+      <Stack.Screen name="ServerScreen" options={{ headerShown: false, gestureEnabled: false }} />
+      <Stack.Screen name="TableScreen" options={{ headerShown: false, gestureEnabled: false }} />
     </Stack>
   );
 }
