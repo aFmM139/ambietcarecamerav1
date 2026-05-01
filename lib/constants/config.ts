e77e7de4c642cf7d3b1eb11b98fb964334ed6d57
aFmM139 <andyfajardo13092008@gmail.com>
2026-05-01 00:53:10 -0500

    ♻️refactor:Se refactoriza el codigo -Se refactoriza el codigo para que los IPS se vean mas limpios

--- lib/constants/config.ts
+++ lib/constants/config.ts
@@ -1,11 +1,13 @@
 export const CAM_IP = "192.168.1.8";
 export const CAM_URL = `http://${CAM_IP}`;
 
-export const SENSOR_IP = "192.168.1.10";
-
+export const SENSOR_IP = "192.168.1.11";
 export const SENSOR_1_URL = `http://${SENSOR_IP}/sensor1`;
 export const SENSOR_2_URL = `http://${SENSOR_IP}/sensor2`;
 export const AIRE_URL = `http://${SENSOR_IP}/aire`;
+
+export const SERVO_IP = "192.168.1.11";
+export const SERVO_URL = `http://${SERVO_IP}/servo`;
 
 export const CAR_IP = "192.168.1.6";
 export const CAR_URL = `http://${CAR_IP}`;
